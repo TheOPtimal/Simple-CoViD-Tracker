71c7f2a230a0ce918c5ea86f5c4f54dc829d98bf
Optimalgitit <jggsecondary@gmail.com>
2020-05-02 23:16:46 +0400

    State of the art responsive design

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Loading.css";
 
+// eslint-disable-next-line
 function Spinner() {
   return (
     <div className="lds-spinner">
@@ -25,7 +26,9 @@ export default function Loading({ visibility }: { visibility: boolean }) {
     return (
       <div className="modal-bg">
         <div className="modal-box">
-          <span className="modal-text">Looks like the hourly quota has been reached. Come back in an hour!</span>
+          <span className="modal-text">
+            Looks like the hourly quota has been reached. Come back in an hour!
+          </span>
           {/* <Spinner /> */}
         </div>
       </div>
